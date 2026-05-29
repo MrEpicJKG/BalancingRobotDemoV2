c1f5f62beb75388141685b846203159c64883bd1
John Gerken <70169793+MrEpicJKG@users.noreply.github.com>
2026-05-29 14:09:33 -0400

    Added Gyro Calibration

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from ucollections import namedtuple
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, InfraredSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button
+from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
@@ -44,12 +44,13 @@ lightFlashOffTime = 90              # int, Num millisecs for the bricks light to
 # Write your program here.
 def CalibrateGyro():
 #{
+    ev3.speaker.set_volume(100)
     lightIsOn = False
     ev3.speaker.say("Preparing for gyro calibration. Please place me in my stand and press the center button when ready.")
     #Flash the light yellow every 0.5 seconds until the center button is pressed.
     lightFlashTimer.reset()
     ev3.light.off()
-    while ev3.Buttons.pressed() != [CENTER]:
+    while Button.CENTER not in ev3.buttons.pressed():
     #{
         timeVal = lightFlashTimer.time()   #get the current timer val
         #if the light is OFF and has been so for the set amount of time
@@ -57,8 +58,8 @@ def CalibrateGyro():
         #{
             # reset the timer, turn the light ON, and toggle the variable
             lightFlashTimer.reset()
-            ev3.light.on(YELLOW)
-            lightIsOn == True
+            ev3.light.on(Color.YELLOW)
+            lightIsOn = True
         #}
 
         #if the light is ON and it has been so for the set amount of time
@@ -67,9 +68,46 @@ def CalibrateGyro():
             # reset the timer, turn the light OFF, and toggle the variable
             lightFlashTimer.reset()
             ev3.light.off()
-            lightIsOn == False
+            lightIsOn = False
         #}
     #}
+    ev3.speaker.say("Do Not Touch Me Unitl I say I Am Done! Calibrating Gyro in 3...")
+    wait(100)
+    ev3.speaker.say("2...")
+    wait(100)
+    ev3.speaker.say("1...")
 
-    
+    ######  ACTUAL GYRO CALIBRATION ######
+    while True:
+    #{
+        # Track the change in the Gyro value over time to get an offset value that can
+        # be used to account for gyro drift later
+        gyroMinRate = 440   # default to the max value, so we can go down from there
+        gyroMaxRate = -440   # default to the min value, so we can go up from there
+        gyroSum = 0
+        for _ in range(GYRO_CALIBRATION_LOOP_COUNT):    # loop through the calibration to provide a period of time for the value to accumulate
+        #{
+            gyroSensorValue = gyroSensor.speed()        # get the rotational speed of the gyro (ideally 0, but looking for gyro drift)
+            gyroSum += gyroSensorValue                  # add any drift we find above to a accumulator value
+            if gyroSensorValue > gyroMaxRate:           # if the sensor value is greater than the highest previously recorded value...
+            #{
+                gyroMaxRate = gyroSensorValue           # ...update the previously recorded value to the current value
+            #}
+            if gyroSensorValue < gyroMinRate:           # if the sensor alue is less than the lowest previously recorded value...
+            #{
+                gyroMinRate = gyroSensorValue           # ...update the previously recorded value to the current value
+            #}
+            wait(5)                                     # wait 5 ms
+        #}
+        if gyroMaxRate - gyroMinRate < 2:               # if the accumulated gyro drift is less than 2 deg / sec
+        #{
+            break                                       # break out of the while loop
+        #}
+    #}
+    ###### END OF ACTUAL GYRO CALIBRATION ######
+
+    wait(20)
+    ev3.speaker.say("Calibration Complete. Thank you for waiting. Please remove me from my stand now and set me on the ground.")
 #}
+
+CalibrateGyro()
